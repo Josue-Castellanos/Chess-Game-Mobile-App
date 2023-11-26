a306296c13de0f953dc16530a4809306c478a2bb
Josue-Castellanos <jcastellanos@horizon.csueastbay.edu>
2023-11-25 18:17:05 -0800

    Added loops to provide the possible movements a Bishop piece can make depending on its current position on the board.

--- app/src/main/java/com/example/chessgame/King.java
+++ app/src/main/java/com/example/chessgame/King.java
@@ -1,7 +1,33 @@
 package com.example.chessgame;
 
+import java.util.ArrayList;
+
 public class King extends Piece{
     public King(boolean isWhite, int imageResourceId) {
         super(isWhite, imageResourceId);
+    }
+
+    public ArrayList<Positions> ValidPositions(Positions position) {
+        int col = position.getCol();
+        int row = position.getRow();
+        ArrayList<Positions> validPositions = new ArrayList<>();
+        Positions vp;
+
+
+        // King moves
+        int[] moveRows = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] moveCols = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int i = 0; i < moveRows.length; i++) {
+            int newRow = row + moveRows[i];
+            int newCol = col + moveCols[i];
+
+            if (row >= 0 && row < 8 && col >= 0 && col < 8) {
+                vp = new Positions(newRow, newCol);
+                validPositions.add(vp);
+            }
+        }
+
+        return validPositions;
     }
 }
