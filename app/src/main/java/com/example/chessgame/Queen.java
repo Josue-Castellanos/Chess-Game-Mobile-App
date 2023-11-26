f0e22a714fabfebe62ef095e80d7f2a3b80a1137
Josue-Castellanos <jcastellanos@horizon.csueastbay.edu>
2023-11-25 18:14:51 -0800

    Added loops to provide the possible movements a Rook piece can make depending on its current position on the board.

--- app/src/main/java/com/example/chessgame/Queen.java
+++ app/src/main/java/com/example/chessgame/Queen.java
@@ -1,5 +1,7 @@
 package com.example.chessgame;
 
+import java.util.ArrayList;
+
 public class Queen extends Piece {
 
     public Queen(boolean isWhite, int imageResourceId) {
@@ -7,4 +9,67 @@ public class Queen extends Piece {
     }
 
     // Additional methods and properties specific to the queen can be added
+    public ArrayList<Positions> ValidPositions(Positions position) {
+        int col = position.getCol();
+        int row = position.getRow();
+        ArrayList<Positions> validPositions = new ArrayList<>();
+        Positions vp;
+
+        // Loop for movements towards the right
+        for (int i = col; i < 8; i++) {
+            int newCol = i;
+            vp = new Positions(row, newCol);
+            validPositions.add(vp);
+        }
+        // Loop for movements towards the left
+        for (int i = col; i > 0; i--) {
+            int newCol = i;
+            vp = new Positions(row, newCol);
+            validPositions.add(vp);
+        }
+        // Loop for movements towards the bottom
+        for (int i = row; i < 8; i++) {
+            int newRow = i;
+            vp = new Positions(newRow, col);
+            validPositions.add(vp);
+        }
+        // Loop for movements towards the top
+        for (int i = row; i >= 0; i--) {
+            int newRow = i;
+            vp = new Positions(newRow, col);
+            validPositions.add(vp);
+        }
+        // Loop for diagonal movements towards the top-left
+        for (int i = 1; row - i >= 0 && col - i >= 0; i++) {
+            int newRow = row - i;
+            int newCol = col - i;
+            vp = new Positions(newRow, newCol);
+            validPositions.add(vp);
+        }
+
+        // Loop for diagonal movements towards the top-right
+        for (int i = 1; row - i >= 0 && col + i < 8; i++) {
+            int newRow = row - i;
+            int newCol = col + i;
+            vp = new Positions(newRow, newCol);
+            validPositions.add(vp);
+        }
+
+        // Loop for diagonal movements towards the bottom-left
+        for (int i = 1; row + i < 8 && col - i >= 0; i++) {
+            int newRow = row + i;
+            int newCol = col - i;
+            vp = new Positions(newRow, newCol);
+            validPositions.add(vp);
+        }
+
+        // Loop for diagonal movements towards the bottom-right
+        for (int i = 1; row + i < 8 && col + i < 8; i++) {
+            int newRow = row + i;
+            int newCol = col + i;
+            vp = new Positions(newRow, newCol);
+            validPositions.add(vp);
+        }
+        return validPositions;
+    }
 }
--- app/src/main/java/com/example/chessgame/Rook.java
+++ app/src/main/java/com/example/chessgame/Rook.java
@@ -1,7 +1,44 @@
 package com.example.chessgame;
 
+import java.util.ArrayList;
+
 public class Rook extends Piece{
     public Rook(boolean isWhite, int imageResourceId) {
         super(isWhite, imageResourceId);
+    }
+
+    @Override
+    public ArrayList<Positions> ValidPositions(Positions position) {
+        int col = position.getCol();
+        int row = position.getRow();
+        ArrayList<Positions> validPositions = new ArrayList<>();
+        Positions vp;
+
+        // Rooks can move any number of squares, up and down and side to side.
+        // Rooks can move by getting current Row position and have it reach 7 and 0
+
+        // Checking valid squares to the right of the piece
+        for (int i = col; i < 8; i++) {
+            int newCol = i;
+            vp = new Positions(row, i);
+            validPositions.add(vp);
+        }
+        // Checking valid squares to the left of the piece.
+        for (int i = col; i > 0; i--) {
+            vp = new Positions(row, i);
+            validPositions.add(vp);
+        }
+        // Checking valid squares going up
+        for (int i = row; i < 8; i++) {
+            vp = new Positions(i, col);
+            validPositions.add(vp);
+        }
+        // Checking valid squares going down
+        for (int i = row; i >= 0; i--) {
+            vp = new Positions(i, col);
+            validPositions.add(vp);
+        }
+
+        return validPositions;
     }
 }
