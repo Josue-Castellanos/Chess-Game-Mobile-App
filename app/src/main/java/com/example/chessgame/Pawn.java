a97f3b2da420cbdbab37d33127e6d65ad3604e48
Josue-Castellanos <jcastellanos@horizon.csueastbay.edu>
2023-11-27 09:40:39 -0800

    Created If statements to check whether the new Position of a piece is withing range of Chess Board. Also added a method to check whether a piece can be taken diagonally by a pawn piece.

--- app/src/main/java/com/example/chessgame/Pawn.java
+++ app/src/main/java/com/example/chessgame/Pawn.java
@@ -6,51 +6,106 @@ import java.util.ArrayList;
 
 public class Pawn extends Piece{
     boolean isFirstMove = true;
+    int initialMove = 2;
+    int forwardMove = 1;
     public Pawn(boolean isWhite, int imageResourceId, boolean isFirstMove) {
         super(isWhite, imageResourceId);
     }
 
-    // if first move make to be able to go 2 squares forward, after that only 1
     @Override
-    public ArrayList<Positions> ValidPositions(Positions position) {
+    public ArrayList<Positions> ValidPositions(Positions position, Square[][] Board) {
         int col = position.getCol();
         int row = position.getRow();
         ArrayList<Positions> validPositions = new ArrayList<>();
         Positions vp;
 
-        // If pawn is white
+        // WHITE PAWN
         if (isWhite()) {
             // If pawns first move then its valid to take 2 squares forward
             if (isFirstMove) {
-                for (int i = 1; i <= 2; i++) {
+                // Valid two steps forward
+                for (int i = forwardMove; i <= initialMove; i++) {
                     int newRow = row - i;
-                    vp = new Positions(newRow,col);
-                    validPositions.add(vp);
-                    this.isFirstMove = false;
+
+                    // Check if a piece is on front squares, ignore square if so
+                    if (Board[newRow][col].getPiece() != null) {
+                        break;
+                    }
+                    // save valid positions
+                    else if (newRow >= 0 && newRow < 8 && col >= 0 && col < 8) {
+                        vp = new Positions(newRow, col);
+                        validPositions.add(vp);
+                    }
                 }
+                this.isFirstMove = false;
             }
             // Else only take 1 square forward
             else {
-                int newRow = row - 1;
-                vp = new Positions(newRow, col);
-                validPositions.add(vp);
+                // Valid 1 step forward
+                int newRow = row - forwardMove;
+                // If there is a piece in front ignore that square
+                if (Board[newRow][col].getPiece() == null) {
+                    vp = new Positions(newRow, col);
+                    validPositions.add(vp);
+                }
+
+                // Check for capturing diagonally left
+                int leftCaptureCol = col - 1;
+                if (leftCaptureCol >= 0 && Board[newRow][leftCaptureCol].getPiece() != null
+                        && !Board[newRow][leftCaptureCol].getPiece().isWhite()) {
+                    vp = new Positions(newRow, leftCaptureCol);
+                    validPositions.add(vp);
+                }
+
+                // Check for capturing diagonally right
+                int rightCaptureCol = col + 1;
+                if (rightCaptureCol < 8 && Board[newRow][rightCaptureCol].getPiece() != null
+                        && !Board[newRow][rightCaptureCol].getPiece().isWhite()) {
+                    vp = new Positions(newRow, rightCaptureCol);
+                    validPositions.add(vp);
+                }
             }
         }
+        // BLACK PAWN
         else {
             // If pawns first move then its valid to take 2 squares forward
             if (isFirstMove) {
-                for (int i = 1; i <= 2; i++) {
+                for (int i = 1; i <= initialMove; i++) {
                     int newRow = row + i;
-                    vp = new Positions(newRow, col);
-                    validPositions.add(vp);
+                    if (Board[newRow][col].getPiece() != null) {
+                        break;
+                    } else if (newRow >= 0 && newRow < 8 && col >= 0 && col < 8) {
+                        vp = new Positions(newRow, col);
+                        validPositions.add(vp);
+                    }
                     this.isFirstMove = false;
                 }
             }
             // Else only take 1 square forward
             else {
-                int newRow = row + 1;
-                vp = new Positions(newRow, col);
-                validPositions.add(vp);
+                // Valid 1 step forward
+                int newRow = row + forwardMove;
+                // If there is a piece in front ignore that square
+                if (Board[newRow][col].getPiece() == null) {
+                    vp = new Positions(newRow, col);
+                    validPositions.add(vp);
+                }
+
+                // Check for capturing diagonally left
+                int leftCaptureCol = col - 1;
+                if (leftCaptureCol >= 0 && Board[newRow][leftCaptureCol].getPiece() != null
+                        && Board[newRow][leftCaptureCol].getPiece().isWhite() != isWhite()) {
+                    vp = new Positions(newRow, leftCaptureCol);
+                    validPositions.add(vp);
+                }
+
+                // Check for capturing diagonally right
+                int rightCaptureCol = col + 1;
+                if (rightCaptureCol < 8 && Board[newRow][rightCaptureCol].getPiece() != null
+                        && Board[newRow][rightCaptureCol].getPiece().isWhite() != isWhite()) {
+                    vp = new Positions(newRow, rightCaptureCol);
+                    validPositions.add(vp);
+                }
             }
         }
         return validPositions;
